1023263df7f31587eb4052b1bbda42a317f432d3
Max-Jekov <maxzhekov07@gmail.com>
2025-02-23 23:12:21 +0200

    added feedback and certificates section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ const menuMobile = document.querySelector(".header__menu-list");
 menuBtn.addEventListener("click", function () {
   menuMobile.classList.toggle("menu--open");
 });
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperTwo = new Swiper(".certificates__slider", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBeetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
